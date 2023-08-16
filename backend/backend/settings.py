c7240144712d3dcd081ae5181805c0ca26f8a8b4
Кабелка Анна <dr.kabelka@mail.ru>
2023-08-16 22:49:57 +0300

    второй этап

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'foodgram.wsgi:application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv(
-           'DB_ENGINE', default='django.db.backends.postgresql'),
+        'DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
