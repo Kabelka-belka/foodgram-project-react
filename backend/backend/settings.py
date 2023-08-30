6f6e2335f4bc2171d922353fc3cb34bc50c26881
Кабелка Анна <dr.kabelka@mail.ru>
2023-08-30 14:50:00 +0300

    второй этап

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -59,8 +59,8 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv(
             'DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default='postgres'),
-        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'NAME': os.getenv('DB_NAME', default='foodgram_project'),
+        'USER': os.getenv('POSTGRES_USER', default='user_ann'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
         'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT', default=5432)
