1fd62148af6d754b9261aaa8b538d949f08462a1
Кабелка Анна <dr.kabelka@mail.ru>
2023-08-30 15:51:43 +0300

    второй этап

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,15 +57,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv(
-            'DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default='foodgram_project'),
-        'USER': os.getenv('POSTGRES_USER', default='user_ann'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='12345678'),
-        'HOST': os.getenv('DB_HOST', default='db'),
-        'PORT': os.getenv('DB_PORT', default=5432)
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
-}
+} 
 
 
 AUTH_PASSWORD_VALIDATORS = [
